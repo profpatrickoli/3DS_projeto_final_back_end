88143b47d8f58731d9f03a1e2cd4890a66f131f2
profpatrickoli <138583814+profpatrickoli@users.noreply.github.com>
2026-09-11 08:47:56 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ const db = require("./db")
 
 // npm i bcrypt
 const bcrypt = require("bcrypt")
+
+// npm i jsonwebtoken
+const jwt = require("jsonwebtoken")
+
+// npm i dotenv
+const dotenv = require("dotenv")
+dotenv.config()
+
+// npm i cors
+const cors = require("cors")
+app.use(cors())
+
+
 app.post("/cliente", async (req, res) => {
     try {
         const cliente = req.body
@@ -25,13 +38,42 @@ app.post("/cliente", async (req, res) => {
              cliente.email, cliente.senha]
         )
         res.status(201).json({
-            mensagem: "Cliente cadastrado, ID = " + resultado[0].insertId
+            msg: "Cliente cadastrado, ID = " + resultado[0].insertId
         })
     } catch (error) {
         res.status(500).json({erro: error.message})
     }
 })
 
+
+app.post("/login", async (req,res) => {
+    try {
+        const user = req.body
+        const resultado = await db.pool.query(
+            "SELECT id, nome, email, senha FROM cliente WHERE email = ?", [user.email]
+        )
+        const dados_bd = resultado[0][0]
+        if(!dados_bd) {
+            return res.status(401).json({msg: "Email não cadastrado!"})
+        }
+
+        const senha_valida = await bcrypt.compare(user.senha, dados_bd.senha)
+
+        if(!senha_valida) {
+            return res.status(401).json({msg: "Credenciais inválidas!"})
+        }
+
+        const payload = {
+            id: dados_bd.id,
+            email: dados_bd.email
+        } 
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1m' })
+        return res.status(200).json({nome: dados_bd.nome, token: token})
+
+    } catch (error) {
+        res.status(500).json({erro: error.message})
+    }
+})
 
 
 app.listen(port, () => {
